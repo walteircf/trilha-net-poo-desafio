83ea9ee0fbdf792eca884829c7122e1298df8404
Walteir Coelho <wcfreitasf@gmail.com>
2025-07-25 19:06:46 -0300

    Implementa o código do desafio proposto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,30 @@
-namespace DesafioPOO.Models
-{
-    public abstract class Smartphone
-    {
+namespace DesafioPOO.Models {
+    public abstract class Smartphone {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
 
-        public Smartphone(string numero)
+        public Smartphone (string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar ()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao ()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo (string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.Collections.Generic;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO {
+    class Program {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "62981019844", modelo: "Lumia 1020", imei: "1111111111111", memoria: 256);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Telegram");
+
+            Console.WriteLine("Smartphone Iphone:");
+            Smartphone iphone = new Iphone(numero: "62982127455", modelo: "16 Pro Max", imei: "222222222222", memoria: 512);
+            iphone.ReceberLigacao();
+            iphone.Ligar();
+            iphone.InstalarAplicativo("WhatsApp");
+
+        }
+    }
+}
